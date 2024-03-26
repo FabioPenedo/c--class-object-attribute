e28cc2e4da6597bee51d1e226caf7027663f6f82
Penedo <fabiopenedo21@gmail.com>
2024-03-26 14:04:25 -0300

    primeiro commit, classes, objetos, atributos

---
+++ App/Triangulo.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+  public class Triangulo
+  {
+    public double A;
+    public double B;
+    public double C;
+  }
+}
